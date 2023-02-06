9106fd6ee96e55fcd52d9f4dfe8126f072c6cbf3
Parfen0v <parfenovdo@gmail.com>
2023-02-06 17:48:41 +0300

    PlayerTest created

--- test/io/Hexlet/XO/Game/Model/PlayerTest.java
+++ test/io/Hexlet/XO/Game/Model/PlayerTest.java
@@ -8,9 +8,25 @@ class PlayerTest {
 
     @Test
     void getName() {
+        final String inputValue = "Dimon";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue, null);
+
+        final String actualValye = player.getName();
+
+        assertEquals(expectedValue, actualValye);
     }
 
     @Test
     void getFigure() {
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualValye = player.getFigure();
+
+        assertEquals(expectedValue, actualValye);
     }
 }
