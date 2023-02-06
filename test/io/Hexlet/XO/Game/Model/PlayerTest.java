498bb27aced73a5d95408e0369d6f25be551a4f4
Parfen0v <parfenovdo@gmail.com>
2023-02-06 17:36:19 +0300

    Player created

---
+++ test/io/Hexlet/XO/Game/Model/PlayerTest.java
@@ -0,0 +1,16 @@
+package io.Hexlet.XO.Game.Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getFigure() {
+    }
+}
